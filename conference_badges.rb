fe0ac52f2786714d71729860004a73e6e4e77bf7
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-03-22 14:26:31 -0700

    commit tree 1915f934b1945afdd7dfa53ff3adc2f1ff2bb664

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,12 @@
-def badge_maker speaker
+def badge_maker (speaker)
   "Hello, my name is #{speaker}."
 end
 
-def batch_badge_creator list_of_speakers
-  names_of_speakers = list_of_speakers.map{ |speaker| badge_maker speaker }
-  names_of_speakers
+def batch_badge_creator (list_of_speakers)
+  list_of_speakers.map{ |speaker| badge_maker (speaker) }
 end
 
-def assign_rooms speakers
+def assign_rooms (speakers)
   assignment = []
   speakers.each_with_index do |name, index| 
     if index >= 8
@@ -19,9 +18,9 @@ def assign_rooms speakers
   assignment
 end
 
-def printer speakers
-  badges = batch_badge_creator speakers
-  assignments = assign_rooms speakers
+def printer (speakers)
+  badges = batch_badge_creator (speakers)
+  assignments = assign_rooms (speakers)
   badges.each do |badge|
     puts badge
   end
